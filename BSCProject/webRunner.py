2875103303248584c03c94bf39c6d0a82042910a
zoumayaa1 <zoumayaa1@udayton.edu>
2020-05-11 18:56:49 -0500

    update

--- BSCProject/webRunner.py
+++ BSCProject/webRunner.py
@@ -4,16 +4,20 @@ import selenium
 from selenium import webdriver
 
 driver = webdriver.Chrome(executable_path=r"C:\Users\17738\Desktop\chromedriver_win32\chromedriver.exe")
-driver.get("https://tms.ezfacility.com/OneInvoice.aspx?Invoice2ID=73598431")
+site = input("Enter the URL of the source invoice: ")
+driver.get(site)
 loginBox = driver.find_element_by_name('ctl00$ctl00$CphFormBody$cphFormBody$txtUserName')
 passwordBox = driver.find_element_by_name('ctl00$ctl00$CphFormBody$cphFormBody$txtPassword')
-loginBox.send_keys('azoumaya')
-passwordBox.send_keys('')
+username = input("username: ")
+loginBox.send_keys(username)
+password = input("password: ")
+passwordBox.send_keys(password)
 login = driver.find_element_by_name('ctl00$ctl00$CphFormBody$cphFormBody$btn_Login')
 login.click()
 
 rows = len(driver.find_elements_by_xpath('//*[@id="ctl00_ctl00_CphFormBody_cphFormBody_bigBilledItems_gvBilledItems"]/tbody/tr'))
-print(rows)
+# print(rows)
+inv = input("input the target invoice number: ")
 for x in range(0, rows):
     try:
         edit = driver.find_element_by_xpath('//*[@id="ctl00_ctl00_CphFormBody_cphFormBody_bigBilledItems_gvBilledItems_ctl02_btnEdit"]')
@@ -22,7 +26,7 @@ for x in range(0, rows):
         changeButton.click()
         invoiceBox = driver.find_element_by_name('ctl00$ctl00$CphFormBody$cphFormBody$jqdBilledItemEditor$ctl01$ctl05$txtInvoiceNumber')
         invoiceBox.clear()
-        invoiceBox.send_keys('7115')
+        invoiceBox.send_keys(inv)
         saveBox = driver.find_element_by_xpath('//*[@id="ctl00_ctl00_CphFormBody_cphFormBody_jqdBilledItemEditor_ctl01_ctl05_ctl01"]/ul/li[1]/a')
         saveBox.click()
     except:
